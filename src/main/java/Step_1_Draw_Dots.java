bfd92951cec982978d7d92fd5cd2480935b753f9
Shashikant_Shankar <shashi.shankar@ahduni.edu.in>
2025-01-31 10:21:28 +0530

    CSC360-1 Drawing basic shapes  Code Revision, Addition of some more codes, and Refactoring (renamed)

--- src/main/java/Step_1_Draw_Dots.java
+++ src/main/java/Step_1_Draw_Dots.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class DrawDotsExample extends JPanel {
-
+public class Step_1_Draw_Dots extends JPanel
+{
     @Override
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g)
+    {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-
         // Set dot color
         g2d.setColor(Color.RED);
-
         // Draw some dots
         g2d.fillOval(50, 50, 10, 10); // Dot 1
         g2d.fillOval(100, 100, 10, 10); // Dot 2
@@ -22,7 +21,7 @@ public class DrawDotsExample extends JPanel {
     public static void main(String[] args) {
         // Create a JFrame to hold the JPanel
         JFrame frame = new JFrame("Draw Dots Example");
-        DrawDotsExample panel = new DrawDotsExample();
+        Step_1_Draw_Dots panel = new Step_1_Draw_Dots();
 
         frame.add(panel);
         frame.setSize(300, 200);
