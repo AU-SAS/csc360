d3e5e330f088116e798732694258ec72e838bd10
ahuja <177828565+d12ksh@users.noreply.github.com.>
2025-03-18 16:57:00 +0530

    Push

--- Cluster.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-public class Cluster extends JPanel
-{
-    protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setStroke(new BasicStroke(5)); }
-}
