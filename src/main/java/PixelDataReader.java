7389b7aac4b64184c5077bcdf066064cd3466b9b
Paridhi <wunderkerzenmenschen19@gmail.com>
2025-03-28 22:33:58 +0530

    feat: output pixel data

---
+++ src/main/java/PixelDataReader.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class PixelDataReader {
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:/Users/PARIDHI/Downloads/CSI Holi.png");
+            BufferedImage image = ImageIO.read(file);
+            int width = image.getWidth();
+            int height = image.getHeight();
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int pixel = image.getRGB(x, y);
+
+                    // Extract RGB components
+                    int red   = (pixel >> 16) & 0xFF;
+                    int green = (pixel >> 8)  & 0xFF;
+                    int blue  = pixel & 0xFF;
+                    System.out.println("Pixel at (" + x + ", " + y + "): R=" + red + ", G=" + green + ", B=" + blue);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
