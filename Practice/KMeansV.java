98d4dd32456a020b63bf417f4fb1621ba9f8adac
dhruvee <dhruvee.s@ahduni.edu.in>
2025-04-06 04:52:27 +0530

    K means visualization

--- Practice/KMeansV.java
+++ Practice/KMeansV.java
@@ -1,4 +1,7 @@
-import org.apache.commons.math3.ml.clustering.*;
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.apache.commons.math3.ml.clustering.Cluster;
+import org.apache.commons.math3.ml.clustering.DoublePoint;
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,11 +10,10 @@ import java.util.List;
 import java.util.Random;
 
 public class KMeansV extends JPanel {
-
     private List<CentroidCluster<DoublePoint>> clusters;
 
     public KMeansV() {
-        // Generate some random points
+        // Generate random data points
         List<DoublePoint> points = generateRandomPoints(30, 400, 300);
 
         // Apply KMeans clustering with k = 3
@@ -25,12 +27,13 @@ public class KMeansV extends JPanel {
         Random rand = new Random();
 
         for (int i = 0; i < num; i++) {
-            double[] coords = new double[] {
+            double[] coords = new double[]{
                     50 + rand.nextDouble() * (width - 100),
                     50 + rand.nextDouble() * (height - 100)
             };
             points.add(new DoublePoint(coords));
         }
+
         return points;
     }
 
@@ -40,19 +43,16 @@ public class KMeansV extends JPanel {
 
         // Use different colors for each cluster
         Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.MAGENTA};
-
         Graphics2D g2d = (Graphics2D) g;
-        int clusterIndex = 0;
 
+        int clusterIndex = 0;
         for (Cluster<DoublePoint> cluster : clusters) {
             g2d.setColor(colors[clusterIndex % colors.length]);
-
             for (DoublePoint point : cluster.getPoints()) {
                 int x = (int) point.getPoint()[0];
                 int y = (int) point.getPoint()[1];
                 g2d.fillOval(x - 5, y - 5, 10, 10);
             }
-
             clusterIndex++;
         }
     }
@@ -63,7 +63,7 @@ public class KMeansV extends JPanel {
         frame.add(panel);
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null); // center the frame
+        frame.setLocationRelativeTo(null); // Center the frame
         frame.setVisible(true);
     }
 }
