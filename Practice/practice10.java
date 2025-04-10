be86c30f11776c879fa7a00b6f63130e9388eb7b
Your Name <youremail@example.com>
2025-04-11 03:59:48 +0530

    cluster

---
+++ Practice/practice10.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class practice10 extends JPanel {
+    int[][] points = new int[100][2];
+    int[] clusters = new int[100];
+    int k = 3; // Number of clusters
+
+    public practice10() {
+        generateRandomPoints();
+        assignClusters();
+    }
+
+    void generateRandomPoints() {
+        Random rand = new Random();
+        for (int i = 0; i < points.length; i++) {
+            points[i][0] = rand.nextInt(600); // x
+            points[i][1] = rand.nextInt(400); // y
+        }
+    }
+
+    void assignClusters() {
+        Random rand = new Random();
+        for (int i = 0; i < clusters.length; i++) {
+            clusters[i] = rand.nextInt(k); // Randomly assign to 0, 1 or 2
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN};
+        for (int i = 0; i < points.length; i++) {
+            g2.setColor(colors[clusters[i]]);
+            g2.fillOval(points[i][0], points[i][1], 10, 10);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple Cluster Diagram");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650, 450);
+        frame.setLocationRelativeTo(null);
+        frame.setContentPane(new practice10());
+        frame.setVisible(true);
+    }
+}
