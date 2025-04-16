a8d898e59a9e084126c5a67a19750ef3d675ce7c
Your Name <youremail@example.com>
2025-04-16 08:31:23 +0530

    project

--- Practice/Practice1.java
+++ Practice/Practice1.java
@@ -6,35 +6,34 @@ public class Practice1 extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-
         g2d.setColor(Color.RED);
 
-        int x = 150;
-        int[] y = {50, 100, 150, 200, 250, 300};
+        int x = 50; // same x coordinate
+        int[] y = {50, 100, 150, 200, 250, 300}; // y coordinates for points
 
         for (int i = 0; i < 6; i++) {
-            g2d.fillOval(x - 5, y[i] - 5, 10, 10);
+            g2d.fillOval(x - 5, y[i] - 5, 10, 10); // Draw red data points
         }
 
         g2d.setColor(Color.BLUE);
 
+        // First cluster box (without left line)
+        g2d.drawLine(x, y[0], x + 40, y[0]); // top
+        g2d.drawLine(x + 40, y[0], x + 40, y[1]); // right
+        g2d.drawLine(x + 40, y[1], x, y[1]); // bottom
+        g2d.drawLine(x + 50, (y[0] + y[1]) / 2, x + 40, (y[0] + y[1]) / 2); // middle line on right
 
-        g2d.drawLine(x, y[0], x + 40, y[0]);
-        g2d.drawLine(x + 40, y[0], x + 40, y[1]);
-        g2d.drawLine(x + 40, y[1], x, y[1]);
-        g2d.drawLine(x + 50, (y[0] + y[1]) / 2, x - 40, (y[0] + y[1]) / 2);
+        // Second cluster box (without left line)
+        g2d.drawLine(x, y[2], x + 40, y[2]);
+        g2d.drawLine(x + 40, y[2], x + 40, y[3]);
+        g2d.drawLine(x + 40, y[3], x, y[3]);
+        g2d.drawLine(x + 50, (y[2] + y[3]) / 2, x + 40, (y[2] + y[3]) / 2);
 
-
-        g2d.drawLine(x, y[2], x - 40, y[2]);
-        g2d.drawLine(x - 40, y[2], x - 40, y[3]);
-        g2d.drawLine(x - 40, y[3], x, y[3]);
-        g2d.drawLine(x - 50, (y[2] + y[3]) / 2, x - 40, (y[2] + y[3]) / 2);
-
-
-        g2d.drawLine(x, y[4], x - 40, y[4]);
-        g2d.drawLine(x - 40, y[4], x - 40, y[5]);
-        g2d.drawLine(x - 40, y[5], x, y[5]);
-        g2d.drawLine(x - 50, (y[4] + y[5]) / 2, x - 40, (y[4] + y[5]) / 2);
+        // Third cluster box (without left line)
+        g2d.drawLine(x, y[4], x + 40, y[4]);
+        g2d.drawLine(x + 40, y[4], x + 40, y[5]);
+        g2d.drawLine(x + 40, y[5], x, y[5]);
+        g2d.drawLine(x + 50, (y[4] + y[5]) / 2, x + 40, (y[4] + y[5]) / 2);
     }
 
     public static void main(String[] args) {
@@ -43,7 +42,6 @@ public class Practice1 extends JPanel {
         frame.add(panel);
         frame.setSize(300, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
